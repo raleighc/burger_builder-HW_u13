05760d6042331ad5452cbb5de5b792e628306b35
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-09 00:05:20 -0400

    connected to mysql database with heroku.

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE `burgers_db`;
 
 USE `burgers_db`;
 
-CREATE TABLE `allcharacters` (
+CREATE TABLE burgers (
 	id Int( 11 ) AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR( 255) NOT NULL,
 	devoured BOOLEAN,
